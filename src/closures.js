1de59c96cf842117a883698f6c76d1d56ec14fd1
TJ Epperson <tj.epperson070@gmail.com>
2019-05-27 16:42:53 +0900

    Implemented gameGenerator

--- src/closures.js
+++ src/closures.js
@@ -8,7 +8,33 @@ function randomInteger(n) {
   return Math.floor(Math.random() * (n + 1));
 }
 
-function gameGenerator() {}
+function gameGenerator(upper) {
+  let answer = randomInteger(upper);
+  let guesses = 0;
+
+  return {
+    answer: answer,
+    
+    reset: function reset() {    
+    guesses = 0;
+    answer = randomInteger(upper);
+  },
+
+    giveUp: function giveUp() {
+      console.log(`The answer is ${answer}`);
+      reset();
+    },
+
+    guess: function guess(guess) {
+      guesses++;
+      return guess === this.answer;
+    },
+
+    numGuesses: function numGuesses() {
+      return guesses;
+    }
+  }
+};
 
 function accountGenerator(initial) {
   let balance = initial;
@@ -26,4 +52,9 @@ function accountGenerator(initial) {
       return `Your balance is: $${balance}`;
     }
   };
+}
+
+const game = gameGenerator(4);
+for (let i = 0; i < 5; i++) {
+  console.log(game.guess(i));
 }
