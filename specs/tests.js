66cbb01fb359f54d3d3f27fa1654ae3c638a2b27
Wilson Lau <hello@wilsonplau.com>
2019-05-30 16:33:43 +0900

    Implemented tests for reset

--- specs/tests.js
+++ specs/tests.js
@@ -21,14 +21,49 @@ describe("gameGenerator", () => {
     expect(number.length).toBe(1);
   });
 
-  it("should have a reset method", () => {
-    // How do you test for this?
-    expect(false).toBeTruthy();
+  it("should have a reset method generates a game with the same upper bound", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
+    game.reset();
+    const number = [];
+    for (let i = 0; i < bound; i++) {
+      if (game.guess(i)) {
+        number.push(i);
+      }
+    }
+    expect(number.length).toBe(1);
   });
 
-  it("create your own test", () => {
-    expect(false).toBeTruthy();
+  it("should have a reset method that has a different winning number", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
+    const results = [];
+
+    // Che
+    let firstWinningNumber; 
+    for (let i = 0; i < bound; i++) {
+      if (game.guess(i)) { firstWinningNumber = i; }
+    }
+
+    // Reset and the same winning number until it returns false 
+    while (game.guess(firstWinningNumber)) {
+      game.reset();
+      results.push(games.guess(firstWinningNumber));
+    }
+    expect(results).toContain(false);
   });
+
+  it("should have a reset method that resets the guess counter", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
+    game.guess(1);
+    game.guess(2);
+    game.guess(3);
+    expect(game.numGuesses).toEqual(3);
+    game.reset();
+    expect(game.numGuesses).toEqual(0);
+  });
+
 });
 
 describe("accountGenerator", () => {
