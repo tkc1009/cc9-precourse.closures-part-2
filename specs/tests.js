662f3a1edba0ae74a19478de8c9dc9cb9b825170
Wilson Lau <hello@wilsonplau.com>
2019-06-01 10:33:01 +0900

    Refactored out repetitive functions with beforeEach()

--- specs/tests.js
+++ specs/tests.js
@@ -1,4 +1,10 @@
 describe("gameGenerator", () => {
+
+  beforeEach(() => {
+    this.bound = 4;
+    this.game = gameGenerator(this.bound);
+  });
+
   it("should be there", () => {
     expect(gameGenerator).toBeDefined();
     expect(typeof gameGenerator).toBe("function");
@@ -10,11 +16,10 @@ describe("gameGenerator", () => {
   });
 
   it("should have just one winning number", () => {
-    const bound = 4;
-    const game = gameGenerator(bound);
+    
     const number = [];
-    for (let i = 0; i <= bound; i++) {
-      if (game.guess(i)) {
+    for (let i = 0; i <= this.bound; i++) {
+      if (this.game.guess(i)) {
         number.push(i);
       }
     }
@@ -22,152 +27,133 @@ describe("gameGenerator", () => {
   });
 
   it("should have a reset method generates a game with the same upper bound", () => {
-    const bound = 4;
-    const game = gameGenerator(bound);
-    game.reset();
+    this.game.reset();
     const number = [];
-    for (let i = 0; i <= bound; i++) {
-      if (game.guess(i)) {
+    for (let i = 0; i <= this.bound; i++) {
+      if (this.game.guess(i)) {
         number.push(i);
-      }
+      } 
     }
     expect(number.length).toBe(1);
   });
 
   it("should have a reset method that has a different winning number", () => {
-    const bound = 4;
-    const game = gameGenerator(bound);
     const results = [];
 
     let firstWinningNumber; 
-    for (let i = 0; i <= bound; i++) {
-      if (game.guess(i)) { firstWinningNumber = i; }
+    for (let i = 0; i <= this.bound; i++) {
+      if (this.game.guess(i)) { firstWinningNumber = i; }
     }
 
     // Reset and the same winning number until it returns false 
-    while (game.guess(firstWinningNumber)) {
-      game.reset();
-      results.push(game.guess(firstWinningNumber));
+    while (this.game.guess(firstWinningNumber)) {
+      this.game.reset();
+      results.push(this.game.guess(firstWinningNumber));
     }
     expect(results).toContain(false);
   });
 
   it("should have a reset method that resets the guess counter", () => {
-    const bound = 4;
-    const game = gameGenerator(bound);
-    game.guess(1);
-    game.guess(2);
-    game.guess(3);
-    expect(game.numGuesses()).toEqual(3);
-    game.reset();
-    expect(game.numGuesses()).toEqual(0);
+    this.game.guess(1);
+    this.game.guess(2);
+    this.game.guess(3);
+    expect(this.game.numGuesses()).toEqual(3);
+    this.game.reset();
+    expect(this.game.numGuesses()).toEqual(0);
   });
 
   it("should have a give up function that returns the winning number", () => {
-    const bound = 4;
-    const game = gameGenerator(bound);
     let winningNumber; 
-    for (let i = 0; i <= bound; i++) {
-      if (game.guess(i)) { winningNumber = i; }
+    for (let i = 0; i <= this.bound; i++) {
+      if (this.game.guess(i)) { winningNumber = i; }
     }
-    expect(game.giveUp()).toEqual(winningNumber);
+    expect(this.game.giveUp()).toEqual(winningNumber);
 
   });
 
 });
 
 describe("accountGenerator", () => {
+
+  beforeEach(() => {
+    this.initialBalance = 100;
+    this.account = accountGenerator(this.initialBalance);
+  });
+
   it("should be there", () => {
     expect(accountGenerator).toBeDefined();
     expect(typeof accountGenerator).toBe("function");
   });
 
   it("should have a getBalance function that returns the right balance", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
-    expect(account.getBalance()).toEqual(initialBalance);
+    expect(this.account.getBalance()).toEqual(initialBalance);
   });
 
   it("should have a withdraw function that decreases the balance", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const withdrawalAmount = 50;
-    account.withdraw(withdrawalAmount);
-    expect(account.getBalance()).toEqual(initialBalance - withdrawalAmount);
+    this.account.withdraw(withdrawalAmount);
+    expect(this.account.getBalance()).toEqual(this.initialBalance - withdrawalAmount);
   });
 
   it("should have a deposit function that increases the balance", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const depositAmount = 50;
-    account.deposit(depositAmount);
-    expect(account.getBalance()).toEqual(initialBalance + depositAmount);
+    this.account.deposit(depositAmount);
+    expect(this.account.getBalance()).toEqual(this.initialBalance + depositAmount);
   });
 
   it("should return an approved transaction object upon withdrawal of amount less than balance", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const withdrawalAmount = 50;
-    const returnedTransaction = account.withdraw(withdrawalAmount);
+    const returnedTransaction = this.account.withdraw(withdrawalAmount);
     expect(returnedTransaction.type).toEqual("withdrawal")
     expect(returnedTransaction.amount).toEqual(withdrawalAmount)
-    expect(returnedTransaction.before).toEqual(initialBalance)
-    expect(returnedTransaction.after).toEqual(initialBalance - withdrawalAmount)
+    expect(returnedTransaction.before).toEqual(this.initialBalance)
+    expect(returnedTransaction.after).toEqual(this.initialBalance - withdrawalAmount)
     expect(returnedTransaction.status).toEqual("approved")
   })
 
   it("should return an denied transaction object upon withdrawal of amount less than balance", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const withdrawalAmount = 150;
-    const returnedTransaction = account.withdraw(withdrawalAmount);
+    const returnedTransaction = this.account.withdraw(withdrawalAmount);
     expect(returnedTransaction.type).toEqual("withdrawal")
     expect(returnedTransaction.amount).toEqual(withdrawalAmount)
-    expect(returnedTransaction.before).toEqual(initialBalance)
-    expect(returnedTransaction.after).toEqual(initialBalance)
+    expect(returnedTransaction.before).toEqual(this.initialBalance)
+    expect(returnedTransaction.after).toEqual(this.initialBalance)
     expect(returnedTransaction.status).toEqual("denied")
   })
 
   it("should return an approved transaction object upon deposit of any amount", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const depositAmount = 50;
-    const returnedTransaction = account.deposit(depositAmount);
+    const returnedTransaction = this.account.deposit(depositAmount);
     expect(returnedTransaction.type).toEqual("deposit")
     expect(returnedTransaction.amount).toEqual(depositAmount)
-    expect(returnedTransaction.before).toEqual(initialBalance)
-    expect(returnedTransaction.after).toEqual(initialBalance + depositAmount)
+    expect(returnedTransaction.before).toEqual(this.initialBalance)
+    expect(returnedTransaction.after).toEqual(this.initialBalance + depositAmount)
     expect(returnedTransaction.status).toEqual("approved")
   })
 
   it("should have a function to return the transaction history", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
     const transactionHistory = [];
-    transactionHistory.push(account.deposit(10));
-    transactionHistory.push(account.deposit(100));
-    transactionHistory.push(account.withdraw(50));
-    transactionHistory.push(account.withdraw(120));
-    transactionHistory.push(account.deposit(1));
-    const returnedTransactionHistory = account.transactionHistory(5);
+    transactionHistory.push(this.account.deposit(10));
+    transactionHistory.push(this.account.deposit(100));
+    transactionHistory.push(this.account.withdraw(50));
+    transactionHistory.push(this.account.withdraw(120));
+    transactionHistory.push(this.account.deposit(1));
+    const returnedTransactionHistory = this.account.transactionHistory(5);
     for (let i = 0; i < transactionHistory.length; i++) {
       expect(transactionHistory[i]).toEqual(returnedTransactionHistory[i]);
     }
   })
 
   it("should have a function to return the average of successful transactions", () => {
-    const initialBalance = 100;
-    const account = accountGenerator(initialBalance);
+    this.account.deposit(10);
+    this.account.deposit(100);
+    this.account.deposit(20);
+    this.account.deposit(30);
 
-    account.deposit(10);
-    account.deposit(100);
-    account.deposit(20);
-    account.deposit(30);
-
-    account.withdraw(40);
-    account.withdraw(100);
-    account.withdraw(10);
-    account.withdraw(1000);
+    this.account.withdraw(40);
+    this.account.withdraw(100);
+    this.account.withdraw(10);
+    this.account.withdraw(1000);
 
     const expectedDepositAverage = (10 + 100 + 20 + 30) / 4;
     const expectedWithdrawalAverage = (40 + 100 + 10) / 3;
